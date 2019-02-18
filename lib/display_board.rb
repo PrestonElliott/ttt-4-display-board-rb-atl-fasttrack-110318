6df3f628dca6a7ffa58bab2abf69f1ab20344d32
Preston Elliott <preston.elliott24@gmail.com>
2019-02-18 20:33:07 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,17 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+# board = "   "||board = " X "||board = " O "
+
+# def display_board(board)
+#   board = [" "," "," "," "," "," "," "," "," "]
+#   puts "   |   |   "
+#   puts "-----------"
+#   puts "   |   |   "
+#   puts "-----------"
+#   puts "   |   |   "
+#   puts #{board[x]}
+# end
+
+# display_board(#{board[x]})
+
