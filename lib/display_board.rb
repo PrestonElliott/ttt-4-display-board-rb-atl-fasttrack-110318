d1416f735ce3e54232019689c712911549f14262
Preston Elliott <preston.elliott24@gmail.com>
2019-02-18 20:33:42 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,15 +3,22 @@
 
 # board = "   "||board = " X "||board = " O "
 
-# def display_board(board)
-#   board = [" "," "," "," "," "," "," "," "," "]
-#   puts "   |   |   "
-#   puts "-----------"
-#   puts "   |   |   "
-#   puts "-----------"
-#   puts "   |   |   "
-#   puts #{board[x]}
+def display_board(board)
+  board = [" "," "," "," "," "," "," "," "," "]
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts #{board[x]}
+end
+
+display_board(#{board[x]})
+
+
+# def five(a, b, c, d, e)
+#   "I was passed #{a} #{b} #{c} #{d} #{e}"
 # end
-
-# display_board(#{board[x]})
-
+# five(1, 2, 3, 4, 5 )	»	"I was passed 1 2 3 4 5"
+# five(1, 2, 3, *['a', 'b'])	»	"I was passed 1 2 3 a b"
+# five(*(10..14).to_a)	»	"I was passed 10 11 12 13 14"
